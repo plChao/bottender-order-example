91c5b4fa82a63fa16d700f9d36ac7d72ec22f4aa
Alan Chao <poseidon31418926.cs06@g2.nctu.edu.tw>
2020-10-25 07:07:35 +0800

    fix send result

--- actions/統計.js
+++ actions/統計.js
@@ -23,8 +23,7 @@ module.exports = async context => {
   // 稍微排版一下，一行一種物品
   const result = orderNames
     .map(o =>`${o} 有 ${sortedOrders[o].length} 人，分別是 ${sortedOrders[o].join(', ')} 點的`).join('\n');
-  await context.sendText(orderNames || '沒有訂單QQ');
-  
+  await context.sendText(result || '沒有訂單QQ');
   // if(sortedOrders[o].length == 3){
   //     const newmes = `開玩桌游囉!!`; 
   //     await context.sendText(newmes);
