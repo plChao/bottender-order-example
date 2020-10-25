e35917eda936b27220dc03777acee98a523f02df
Alan Chao <poseidon31418926.cs06@g2.nctu.edu.tw>
2020-10-25 08:54:15 +0800

    why

--- actions/下訂單.js
+++ actions/下訂單.js
@@ -46,12 +46,13 @@ module.exports = async (context, match) => {
       .map(o =>`${o} 有 ${sortedOrders[o].length} 人，分別是 ${sortedOrders[o].join(', ')} `).join('\n');
     
     await context.sendText(sortedOrders[o].length);
-    
-    if(sortedOrders[o].length == 3){
+
+    if(sortedOrders[o].length === 3){
       await context.sendText('Invite');
-    } else {
-      await context.sendText('gggg');
     }
+    // else {
+    //   await context.sendText('gggg');
+    // }
     
     await context.sendText(result || '沒有人玩QQ');
 
